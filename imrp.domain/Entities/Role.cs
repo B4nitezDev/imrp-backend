5f027c38e32846681d9b7e9415dc72c1d6cde5e6
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 20:26:16 -0300

    add: entity role

--- imrp.domain/Entities/Role.cs
+++ imrp.domain/Entities/Role.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace imrp.domain.Entities
 {
     public class Role
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Role(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
     }
 }
