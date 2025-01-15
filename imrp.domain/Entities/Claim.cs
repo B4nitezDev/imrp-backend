144bacd81849a20425b74147965307c399323dcc
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 20:31:58 -0300

    add: entity claim

--- imrp.domain/Entities/Claim.cs
+++ imrp.domain/Entities/Claim.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace imrp.domain.Entities
 {
-    public class Claim
+    public class Claim(int id, string name, string description)
     {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
     }
 }
