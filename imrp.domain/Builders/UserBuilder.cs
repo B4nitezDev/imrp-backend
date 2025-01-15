e913d64f3971199eb2ded68300689162777058ef
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 19:33:17 -0300

    add: entities files and add builder base

---
+++ imrp.domain/Builders/UserBuilder.cs
@@ -0,0 +1,68 @@
+﻿using imrp.domain.Entities;
+
+namespace imrp.domain.Builders
+{
+    public class UserBuilder
+    {
+        private User _user = new User();
+
+        public UserBuilder WithId(int id)
+        {
+            _user.Id = id;
+            return this;
+        }
+
+        public UserBuilder WithName(string name)
+        {
+            _user.Name = name;
+            return this;
+        }
+
+        public UserBuilder WithEmail(string email)
+        {
+            _user.Email = email;
+            return this;
+        }
+
+        public UserBuilder WithPassword(string password)
+        {
+            _user.Password = password;
+            return this;
+        }
+
+        public UserBuilder WithPasswordAfter(string passwordAfter)
+        {
+            _user.Password_after = passwordAfter;
+            return this;
+        }
+
+        public UserBuilder WithPasswordSalt(string passwordSalt)
+        {
+            _user.Password_salt = passwordSalt;
+            return this;
+        }
+
+        public UserBuilder AddRole(Role role)
+        {
+            _user.Roles.Add(role);
+            return this;
+        }
+
+        public UserBuilder AddReport(Report report)
+        {
+            _user.Reports.Add(report);
+            return this;
+        }
+
+        public UserBuilder AddInventoryMovement(InventoryMovement inventoryMovement)
+        {
+            _user.InventoryMovement.Add(inventoryMovement);
+            return this;
+        }
+
+        public User Build()
+        {
+            return _user;
+        }
+    }
+}
---
+++ imrp.domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace imrp.domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Password_after { get; set; }
+        public string Password_salt { get; set; }
+        public List<Role> Roles { get; set; } = [];
+        public List<Report> Reports { get; set; } = [];
+        public List<InventoryMovement> InventoryMovement { get; set; } = [];
+
+    }
+}
