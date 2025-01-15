5ba7b683f31f24d168251ea6a2cefe2722a4a782
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 20:29:57 -0300

    add: method managment the claims

--- imrp.domain/Entities/Role.cs
+++ imrp.domain/Entities/Role.cs
@@ -6,12 +6,18 @@ namespace imrp.domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<Claim> Claims { get; set; } = [];
 
         public Role(int id, string name, string description)
         {
             Id = id;
             Name = name;
             Description = description;
+        }
+
+        public void AddClaim(Claim claim)
+        {
+            Claims.Add(claim);
         }
     }
 }
