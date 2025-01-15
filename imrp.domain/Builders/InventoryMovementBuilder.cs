3977bb180256cb9687f4a21b1bc7905522dcfea4
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 20:41:57 -0300

    add: entity inventorymovement and builder

---
+++ imrp.domain/Builders/InventoryMovementBuilder.cs
@@ -0,0 +1,80 @@
+﻿
+using imrp.domain.Entities;
+
+namespace imrp.domain.Builders
+{
+    public class InventoryMovementBuilder
+    {
+        private int _id;
+        private string _sku;
+        private string _productName;
+        private int _productQuantity;
+        private string _movementType;
+        private DateTime _movementDate;
+        private int _productId;
+        private int _userId;
+
+        public InventoryMovementBuilder WithId(int id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithSku(string sku)
+        {
+            _sku = sku;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithProductName(string productName)
+        {
+            _productName = productName;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithProductQuantity(int productQuantity)
+        {
+            _productQuantity = productQuantity;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithMovementType(string movementType)
+        {
+            _movementType = movementType;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithMovementDate(DateTime movementDate)
+        {
+            _movementDate = movementDate;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithProductId(int productId)
+        {
+            _productId = productId;
+            return this;
+        }
+
+        public InventoryMovementBuilder WithUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        public InventoryMovement Build()
+        {
+            return new InventoryMovement
+            {
+                Id = _id,
+                Sku = _sku,
+                ProductName = _productName,
+                ProductQuantity = _productQuantity,
+                MovementType = _movementType,
+                MovementDate = _movementDate,
+                ProductId = _productId,
+                UserId = _userId
+            };
+        }
+    }
+}
--- imrp.domain/Entities/InventoryMovement.cs
+++ imrp.domain/Entities/InventoryMovement.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace imrp.domain.Entities
 {
     public class InventoryMovement
     {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public string MovementType { get; set; }
+        public DateTime MovementDate { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
     }
 }
