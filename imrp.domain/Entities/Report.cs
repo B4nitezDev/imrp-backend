7e03e3193c5df3184d35b53c1439ba613f70a3bf
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 20:44:34 -0300

    add: entity  report

--- imrp.domain/Entities/Report.cs
+++ imrp.domain/Entities/Report.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace imrp.domain.Entities
 {
     public class Report
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
