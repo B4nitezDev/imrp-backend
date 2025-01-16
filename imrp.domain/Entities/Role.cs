c68bed4c5d69d8474c9f39385dd7dea9468d3c5e
B4nitez Dev <benitezdev0101@gmail.com>
2025-01-15 21:16:56 -0300

    add list user

--- imrp.domain/Entities/Role.cs
+++ imrp.domain/Entities/Role.cs
@@ -7,6 +7,7 @@ namespace imrp.domain.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Claim> Claims { get; set; } = [];
+        public List<User> Users { get; set; } = [];
 
         public Role(int id, string name, string description)
         {
@@ -18,6 +19,21 @@ namespace imrp.domain.Entities
         public void AddClaim(Claim claim)
         {
             Claims.Add(claim);
+        }
+
+        public void RemoveClaim(Claim claim)
+        {
+            Claims.Remove(claim);
+        }
+
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+
+        public void RemoveUser(User user)
+        {
+            Users.Remove(user);
         }
     }
 }
