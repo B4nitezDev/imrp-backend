7630e03f4ce07d81a63dd11e9e4fbeab1e158f1b
benitez <ebenitez@bizlogit.net>
2025-01-18 14:03:54 -0300

    agrego dto

---
+++ imrp.application/Dto/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using imrp.domain.Entities;
+
+namespace imrp.application.Dto
+{
+    public class UserDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Password_after { get; set; } = string.Empty;
+        public int Password_salt { get; set; }
+        public List<UserRole>? UserRoles { get; set; }
+        public List<Report>? Reports { get; set; }
+        public List<InventoryMovement>? InventoryMovement { get; set; }
+    }
+}
