976aae0fb8dc9fd6ed6faf2457a57b96d8390635
benitez <ebenitez@bizlogit.net>
2025-01-18 16:20:38 -0300

    add: UserController.cs

---
+++ imrp-backend/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using imrp.application.Dto;
+using imrp.application.Interfaces.UseCases.User;
+using imrp.application.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace imrp_backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(IAddUserUseCase addUserUseCase) : ControllerBase
+{
+    private IAddUserUseCase _addUserUseCase = addUserUseCase;
+
+    [HttpPost]
+    public IActionResult AddUser(UserDto userDto)
+    {
+        var result = _addUserUseCase.Execute(userDto);
+        return Ok(result.Value);
+    }
+}
