fd8183663512ae2a1e44571e0afecd20a9461fb2
benitez <ebenitez@bizlogit.net>
2025-01-18 14:23:23 -0300

    Add: logger

--- imrp-backend/Program.cs
+++ imrp-backend/Program.cs
@@ -1,14 +1,27 @@
+using imrp.application.Services;
 using imrp.domain.Interfaces;
 using imrp.persistence.Database;
 using imrp.persistence.Repository;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<IrmpDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ImrpDbContext")));
 
+// LOgs
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/logs.txt", rollingInterval: RollingInterval.Day)
+    .MinimumLevel.Debug()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+
 // Add services to the container.
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddSingleton<ILoggerService, LoggerService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
---
+++ imrp.application/Services/LoggerService.cs
@@ -0,0 +1,50 @@
+﻿
+using Serilog;
+
+namespace imrp.application.Services
+{
+    public interface ILoggerService
+    {
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception? ex);
+        void LogDebug(string message);
+    }
+
+    public class LoggerService: ILoggerService
+    {
+        private readonly ILogger _logger;
+
+        public LoggerService()
+        {
+            _logger = Log.Logger;
+        }
+
+        public void LogInformation(string message)
+        {
+            _logger.Information(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            _logger.Warning(message);
+        }
+
+        public void LogError(string message, Exception? ex)
+        {
+            if (ex != null)
+            {
+                _logger.Error(ex, message);
+            }
+            else
+            {
+                _logger.Error(message);
+            }
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+    }
+}
