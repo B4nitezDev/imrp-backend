5252f1df7dd9caba4106ea6a8137d645dd78b892
benitez <ebenitez@bizlogit.net>
2025-01-18 16:20:31 -0300

    add: agrego use case

---
+++ imrp.application/Interfaces/UseCases/User/IAddUserUseCase.cs
@@ -0,0 +1,8 @@
+﻿using imrp.application.Dto;
+
+namespace imrp.application.Interfaces.UseCases.User;
+
+public interface IAddUserUseCase
+{
+    Result.Result<UserDto> Execute(UserDto userDto);
+}
---
+++ imrp.application/Use cases/User/AddUserUseCase.cs
@@ -0,0 +1,15 @@
+﻿using imrp.application.Dto;
+using imrp.application.Services;
+
+namespace imrp.application.Use_cases.User;
+
+public class AddUserUseCase(UserServices userServices)
+{
+    private readonly UserServices _userServices = userServices;
+    
+    public Result.Result<UserDto> Execute(UserDto userDto)
+    {
+        // TODO: add password hashing
+        return _userServices.Add(userDto);
+    }
+}
