a13485b94b77ef519e90673537696b81824125f6
B4nitez Dev <benitezdev0101@gmail.com>
2025-02-08 12:03:44 -0300

    chore: feature de product aun no terminada

---
+++ imrp.application/Interfaces/UseCases/Product/IAddProduct.cs
@@ -0,0 +1,10 @@
+﻿
+using imrp.application.Dto;
+
+namespace imrp.application.Interfaces.UseCases.Product
+{
+    public interface IAddProduct
+    {
+        public Result.Result<AddProductDto> Execute(AddProductDto product);
+    }
+}
---
+++ imrp.application/Use cases/Prouct/AddProduct.cs
@@ -0,0 +1,17 @@
+﻿
+using imrp.application.Dto;
+using imrp.application.Interfaces.UseCases.Product;
+using imrp.application.Result;
+
+namespace imrp.application.Use_cases.Prouct
+{
+    public class AddProduct: IAddProduct
+    {
+
+
+        public Result.Result<AddProductDto> Execute(AddProductDto dto)
+        {
+            
+        }
+    }
+}
